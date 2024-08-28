acac38585583c8731649b821443cff350217e74d
Eliel de Souza Santos <eliel.santos.dev@gmail.com>
2024-08-28 17:25:41 -0300

    Sreenmatch - OpenAI: adicionando feat para traduzir a sinopse por meio de IA com a api da OpenAI

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.helpers.Categoria;
+import br.com.alura.screenmatch.service.ConsultaChatGPT;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import org.springframework.util.StringUtils;
 
@@ -21,7 +22,7 @@ public class Serie {
         this.totalTemporadas = dadosSerie.totalTemporadas();
         this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
         this.poster = dadosSerie.poster();
-        this.sinopse = dadosSerie.sinopse();
+        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse());
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0]);
         this.atores = dadosSerie.atores();
 
